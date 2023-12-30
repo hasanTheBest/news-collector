ab41f842f264eda8a9e50a07a063d127b5a534b3
Mahmud <rainbowmahmud@gmail.com>
2023-12-30 23:22:33 +0600

    Initial setup done without database

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+
+const app = express();
+
+// Define middleware to parse incoming requests as JSON
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Everything Fine");
+});
+
+// scraping here
+app.post("/news", async (req, res) => {
+  try {
+    const { urls } = req.body; // Assuming URLs are sent in the request body
+
+    const browser = await puppeteer.launch({
+      defaultViewport: {
+        width: 1920,
+        height: 1080,
+      },
+      headless: "new",
+    });
+    const page = await browser.newPage();
+
+    const scrapedData = [];
+
+    for (const url of urls) {
+      await page.goto(url, {
+        waitUntil: "domcontentloaded",
+      });
+      // Perform scraping logic here and push the scraped data to the array
+      // Example:
+      const title = await page.title();
+      scrapedData.push({ url, title });
+    }
+
+    await browser.close();
+
+    // Save scraped data to MongoDB using Mongoose
+    // Assuming you have a MongoDB schema defined as 'ScrapedData' with fields 'url' and 'title'
+    // Configure Mongoose to connect to your MongoDB instance before this step
+
+    // const ScrapedData = require("./models/ScrapedData"); // Import Mongoose model
+
+    // await ScrapedData.insertMany(scrapedData);
+
+    res.status(200).json({
+      message: "Scraping 'and saving to MongoDB' successful",
+      data: scrapedData,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error", message: error });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
